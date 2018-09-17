c041b89e346e970981abc469e23e2a9a98628c22
Mohos Tamás <tomi@mohos.name>
2018-09-17 16:22:21 +0200

    feat(DailyMenu): add ConfigProvider class

---
+++ src/DailyMenu/ConfigProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DailyMenu;
+
+use Psr\Container\ContainerInterface;
+use Slim\App;
+
+/**
+ * Class ConfigProvider
+ * @package App\DailyMenu
+ */
+class ConfigProvider
+{
+    public function __invoke(App $app)
+    {
+        $this->loadDependencies($app->getContainer());
+    }
+
+    public function loadDependencies(ContainerInterface $container)
+    {
+        $container['pdo'] = function () {
+            $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', getenv('DB_HOST'), getenv('DB_NAME'));
+            $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASSWORD'));
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        };
+    }
+}
