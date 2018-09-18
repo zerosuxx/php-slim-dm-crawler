dde8f549063979ce3e79e50cb210bb015a9c4298
Mohos Tamás <tomi@mohos.name>
2018-09-18 16:15:44 +0200

    fix(BonnieCrawlerTest): createBonnieCrawler first parameter add default value

--- test/App/Crawler/BonnieCrawlerTest.php
+++ test/App/Crawler/BonnieCrawlerTest.php
@@ -15,7 +15,7 @@ class BonnieCrawlerTest extends DailyMenuTestCase
      */
     public function getDailyMenu_GivenTodayDateTimeParameter_ReturnsCurrentDailyMenu()
     {
-        $menu = $this->createBonnieCrawler('bonnie_daily_menu_18_09_17-21.html')->getDailyMenu(new DateTime('2018-09-17'));
+        $menu = $this->createBonnieCrawler()->getDailyMenu(new DateTime('2018-09-17'));
         $this->assertInstanceOf(Menu::class, $menu);
         $this->assertEquals(['Zöldbableves', 'Rántott csirkemell salátával'], $menu->getFoods());
         $this->assertEquals(1350, $menu->getPrice());
@@ -41,7 +41,7 @@ class BonnieCrawlerTest extends DailyMenuTestCase
         $this->createBonnieCrawler()->getDailyMenu(new DateTime('2018-09-22'));
     }
 
-    private function createBonnieCrawler($assetFile) {
+    private function createBonnieCrawler($assetFile = 'bonnie_daily_menu_18_09_17-21.html') {
         $file = __DIR__ . '/assets/' . $assetFile;
         $url = BonnieCrawler::DEFAULT_URL;
         $clientMock = $this->createClientMock($file, $url);
