fd3a51e7683f49f6ceea1f54caca154a2f044c1a
Mohos Tamás <tomi@mohos.name>
2018-09-18 15:58:59 +0200

    feat(DailyMenu): add some dependencies and config

--- src/DailyMenu/ConfigProvider.php
+++ src/DailyMenu/ConfigProvider.php
@@ -2,9 +2,16 @@
 
 namespace App\DailyMenu;
 
+use App\DailyMenu\Crawler\BonnieCrawler;
+use App\DailyMenu\Crawler\CrawlerFactory;
+use App\DailyMenu\Dao\MenusDao;
+use App\DailyMenu\Dao\RestaurantsDao;
+use App\DailyMenu\Service\SaveDailyMenuService;
+use GuzzleHttp\Client;
 use PDO;
 use Psr\Container\ContainerInterface;
 use Slim\App;
+use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * Class ConfigProvider
@@ -15,6 +22,16 @@ class ConfigProvider
     public function __invoke(App $app)
     {
         $this->loadDependencies($app->getContainer());
+        $this->loadConfig($app->getContainer());
+    }
+
+    public function loadConfig(ContainerInterface $container)
+    {
+        $container['app_config'] = [
+            'crawler_aliases' => [
+                'Bonnie' => BonnieCrawler::class
+            ]
+        ];
     }
 
     public function loadDependencies(ContainerInterface $container)
@@ -24,6 +41,33 @@ class ConfigProvider
             $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASSWORD'));
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
+        };
+        $container['Client'] = function () {
+            return new Client();
+        };
+        $container['DomCrawler'] = function () {
+            return new Crawler();
+        };
+        $container[CrawlerFactory::class] = function (ContainerInterface $container) {
+            return new CrawlerFactory(
+                $container->get(RestaurantsDao::class),
+                $container->get('Client'),
+                $container->get('DomCrawler')
+            );
+        };
+        $container[MenusDao::class] = function (ContainerInterface $container) {
+            return new MenusDao($container->get('pdo'));
+        };
+        $container[RestaurantsDao::class] = function (ContainerInterface $container) {
+            $appConfig = $container->get('app_config');
+            return new RestaurantsDao($container->get('pdo'), $appConfig['crawler_aliases']);
+        };
+        $container[SaveDailyMenuService::class] = function (ContainerInterface $container) {
+            return new SaveDailyMenuService(
+                $container->get(RestaurantsDao::class),
+                $container->get(MenusDao::class),
+                $container->get(CrawlerFactory::class)
+            );
         };
     }
 }
