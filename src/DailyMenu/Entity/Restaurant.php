1c257690cbeabd6a1f299e32a61e9b9bf4f0d6bb
Mohos Tamás <tomi@mohos.name>
2018-09-18 13:43:12 +0200

    feat(DailyMenu): add Restaurant entity and getName method

---
+++ src/DailyMenu/Entity/Restaurant.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DailyMenu\Entity;
+
+/**
+ * Class Restaurant
+ * @package App\DailyMenu\Entity
+ */
+class Restaurant
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
---
+++ test/App/Entity/RestaurantTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Test\App\Entity;
+
+use App\DailyMenu\Entity\Restaurant;
+use PHPUnit\Framework\TestCase;
+
+class RestaurantTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function getName_ReturnsName()
+    {
+        $restaurant = new Restaurant('Test name');
+        $this->assertEquals('Test name', $restaurant->getName());
+    }
+
+}
