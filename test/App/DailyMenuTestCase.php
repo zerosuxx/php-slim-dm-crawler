e632183ecbb206c136d7d21f67945f435ab0deb4
Mohos Tamás <tomi@mohos.name>
2018-09-18 15:32:48 +0200

    feat(DailyMenuApp): add DailyMenuTestCase class

---
+++ test/App/DailyMenuTestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Test\App;
+
+use App\AppBuilder;
+use App\DailyMenu\ConfigProvider;
+use App\TestSuite\AbstractSlimTestCase;
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * Class DailyMenuTestCase
+ * @package Test\App
+ */
+class DailyMenuTestCase extends AbstractSlimTestCase
+{
+
+    /**
+     * @param AppBuilder $appBuilder
+     * @return void
+     */
+    protected function addProvider(AppBuilder $appBuilder)
+    {
+        $appBuilder->addProvider(new ConfigProvider());
+    }
+
+    protected function createClientMock(string $file, $url) {
+        $bodyMock = $this->createMock(StreamInterface::class);
+        $bodyMock
+            ->expects($this->once())
+            ->method('__toString')
+            ->willReturn(file_get_contents($file));
+
+        $responseMock = $this->createMock(ResponseInterface::class);
+
+        $responseMock
+            ->expects($this->once())
+            ->method('getBody')
+            ->willReturn($bodyMock);
+
+        $clientMock = $this->createMock(Client::class);
+        $clientMock
+            ->expects($this->once())
+            ->method('request')
+            ->with('GET', $url)
+            ->willReturn($responseMock);
+        return $clientMock;
+    }
+}
