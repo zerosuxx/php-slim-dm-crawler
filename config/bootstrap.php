202423424ae5d3176fb5b132f3aaf5e1a2450805
Mohos Tamás <tomi@mohos.name>
2018-09-20 15:38:30 +0200

    feat(DailyMenuApp): enable error log

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,6 +4,9 @@ use Dotenv\Dotenv;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+ini_set('log_errors', 1);
+ini_set('error_log', dirname(__DIR__) . "/logs/error.log");
+
 $env = getenv('APPLICATION_ENV') ?: 'prod';
 
 (new Dotenv(__DIR__ . '/environment', '.env.' . $env))->load();
