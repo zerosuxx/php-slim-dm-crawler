290835d84c1b2d91f6d6a6337464da5ee3128b6d
Mohos Tamás <tomi@mohos.name>
2018-09-20 16:21:55 +0200

    feat(save_daily_menus.php): fix import

--- bin/save_daily_menus.php
+++ bin/save_daily_menus.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\DailyMenu\Service\SaveDailyMenusService;
 use Slim\App;
 
 require_once dirname(__DIR__) . '/config/bootstrap.php';
 
 (function(App $app) {
     $container = $app->getContainer();
-    /* @var $saveDailyMenuService \App\DailyMenu\Service\SaveDailyMenuService */
-    $saveDailyMenuService = $container->get(\App\DailyMenu\Service\SaveDailyMenuService::class);
+    /* @var $saveDailyMenuService SaveDailyMenusService */
+    $saveDailyMenuService = $container->get(SaveDailyMenusService::class);
     $saveDailyMenuService->saveDailyMenus(new DateTime());
 })(require dirname(__DIR__) . '/config/app.php');
