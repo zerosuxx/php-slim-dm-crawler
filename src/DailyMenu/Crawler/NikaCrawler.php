15a71ded1d8abd429a60b3ead91a7337d734ff82
Mohos Tamás <tomi@mohos.name>
2018-09-24 11:51:53 +0200

    feat(DailyMenu): add Nika crawler

---
+++ src/DailyMenu/Crawler/NikaCrawler.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\DailyMenu\Crawler;
+
+use App\DailyMenu\Entity\Menu;
+use App\DailyMenu\Entity\Restaurant;
+use DateTime;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Class NikaCrawler
+ * @package App\DailyMenu\Crawler
+ */
+class NikaCrawler extends AbstractCrawler
+{
+    /**
+     * @param Restaurant $restaurant
+     * @param DateTime $date
+     * @param Crawler $domCrawler
+     * @return Menu
+     */
+    protected function createMenu(Restaurant $restaurant, DateTime $date, Crawler $domCrawler): Menu
+    {
+        $dayNum = null;
+        $postIndex = null;
+        $posts = $domCrawler->filter('[data-sigil="expose"]');
+        foreach($posts as $index => $postElement) {
+            if(strpos($postElement->textContent, $date->format('Y.m.d') . '-') !== false) {
+                $dayNum = 0;
+            } else if(strpos($postElement->textContent, '-' . $date->format('m.d.')) !== false) {
+                $dayNum = 1;
+            }
+            if(null !== $dayNum) {
+                $postIndex = $index;
+                break;
+            }
+        }
+
+        if(!$postIndex) {
+            throw new \InvalidArgumentException('Daily menu not found for this date');
+        }
+
+        $post = $posts->eq($postIndex);
+
+        $menuData = $this->getMenuData($post, $dayNum);
+
+        $foods = array_slice(explode(' - ', $menuData[0]->textContent), 1);
+        $dessert = explode(' - ', $menuData[1]->textContent, 2);
+        $foods[] = 'Desszert: ' . $dessert[1];
+
+        return new Menu($restaurant->getId(), $foods, $this->getPrice($post), $date);
+    }
+
+    private function getMenuData(Crawler $post, int $dayNum)
+    {
+        $menus = array_slice(iterator_to_array($post->filter('p')->getIterator()), 1);
+        $dayChunks = array_chunk($menus, 2);
+        return $dayChunks[$dayNum];
+    }
+
+    private function getPrice(Crawler $post): int
+    {
+        $priceMatches = [];
+        preg_match('/Ár:.*([0-9]+)Ft/Us', $post->text(), $priceMatches);
+        return (int)$priceMatches[1];
+    }
+}
---
+++ test/DailyMenu/Crawler/NikaCrawlerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Test\DailyMenu\Crawler;
+
+use App\DailyMenu\Crawler\NikaCrawler;
+use App\DailyMenu\Entity\Restaurant;
+use DateTime;
+use Test\DailyMenu\DailyMenuSlimTestCase;
+
+class NikaCrawlerTest extends DailyMenuSlimTestCase
+{
+    /**
+     * @test
+     */
+    public function getDailyMenu_GivenTodayDateTimeParameter_ReturnsCurrentDailyMenu()
+    {
+        $menu = $this->createCrawler()->getDailyMenu(new \DateTime('2018-09-24'));
+
+        $this->assertEquals([
+            'Bableves',
+            'Lasagne',
+            'Mézes mustáros csirkemell jázmin rizzsel',
+            'Desszert: Oreo kehely (menühöz választható - 300Ft)'
+        ], $menu->getFoods());
+        $this->assertEquals(1290, $menu->getPrice());
+    }
+
+    /**
+     * @test
+     */
+    public function getDailyMenu_GivenTomorrowDateTimeParameter_ReturnsCurrentDailyMenu()
+    {
+        $menu = $this->createCrawler()->getDailyMenu(new \DateTime('2018-09-25'));
+
+        $this->assertEquals([
+            'tervezés alatt',
+            'Rántott csirkemell petrezselymes burgonyával',
+            'Sárgaborsófőzelék sült debrecenivel',
+            'Desszert: ? (menühöz választható - 300Ft)'
+        ], $menu->getFoods());
+        $this->assertEquals(1290, $menu->getPrice());
+    }
+
+    /**
+     * @test
+     */
+    public function getDailyMenu_GivenInvalidDateTimeParameter_ThrowsException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->createCrawler()->getDailyMenu(new DateTime('2018-09-26'));
+    }
+
+    private function createCrawler($assetFile = 'nika_daily_menu_18_09_24-25.html') {
+        $file = __DIR__ . '/assets/' . $assetFile;
+        $clientMock = $this->createClientMock($file);
+        return new NikaCrawler($clientMock, $this->getService('domCrawler'), new Restaurant('Nika', '', 1));
+    }
+
+}
