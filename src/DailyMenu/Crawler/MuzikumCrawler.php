df88405740795272654572a0d1195833450df3c3
Mohos Tamás <tomi@mohos.name>
2018-09-24 15:01:18 +0200

    feat(DailyMenuApp): add MuzikumCrawler class

---
+++ src/DailyMenu/Crawler/MuzikumCrawler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DailyMenu\Crawler;
+
+use App\DailyMenu\Entity\Menu;
+use App\DailyMenu\Entity\Restaurant;
+use DateTime;
+use InvalidArgumentException;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Class MuzikumCrawler
+ * @package App\DailyMenu\Crawler
+ */
+class MuzikumCrawler extends AbstractCrawler
+{
+    protected function createMenu(Restaurant $restaurant, DateTime $date, Crawler $domCrawler): Menu
+    {
+        $dayOfWeek = $date->format('N');
+        $menuContent = $domCrawler->filter('.content-right div p')->eq($dayOfWeek-1)->html();
+
+        $menuData = explode('<br>', $menuContent);
+
+        if(count($menuData) < 2) {
+            throw new InvalidArgumentException('Daily menu not found for this date');
+        }
+
+        $foods = $this->filterMenuData($menuData);
+
+        $priceMatches = [];
+        preg_match('/A menü ára ([0-9]+)/', $domCrawler->text(), $priceMatches);
+
+        return new Menu($restaurant->getId(), $foods, $this->getPrice($domCrawler), $date);
+    }
+
+    private function getPrice(Crawler $domCrawler)
+    {
+        $priceMatches = [];
+        preg_match('/A menü ára ([0-9]+)/', $domCrawler->text(), $priceMatches);
+        return (int)$priceMatches[1];
+    }
+
+    private function filterMenuData(array $menuData)
+    {
+        return array_map('trim', $menuData);
+    }
+}
---
+++ test/DailyMenu/Crawler/MuzikumCrawlerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Test\DailyMenu\Crawler;
+
+use App\DailyMenu\Crawler\MuzikumCrawler;
+use DateTime;
+use Test\DailyMenu\DailyMenuSlimTestCase;
+
+class VendiakCrawlerTest extends DailyMenuSlimTestCase
+{
+    /**
+     * @test
+     */
+    public function getDailyMenu_GivenTodayDateTimeParameter_ReturnsCurrentDailyMenu()
+    {
+        $menu = $this->createCrawler(
+            MuzikumCrawler::class,
+            'muzikum_daily_menu_18_09_24-28.html'
+        )->getDailyMenu(new \DateTime('2018-09-24'));
+
+        $this->assertEquals([
+            'Francia hagymaleves diós veknivel',
+            'Csirkemell sajttal, sonkával sütve, petrezselymes burgonyával',
+        ], $menu->getFoods());
+        $this->assertEquals(1390, $menu->getPrice());
+    }
+
+    /**
+     * @test
+     */
+    public function getDailyMenu_GivenInvalidDateTimeParameter_ThrowsException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->createCrawler(
+            MuzikumCrawler::class,
+            'muzikum_daily_menu_18_08_21-24.html'
+        )->getDailyMenu(new DateTime('2018-08-20'));
+    }
+
+}
