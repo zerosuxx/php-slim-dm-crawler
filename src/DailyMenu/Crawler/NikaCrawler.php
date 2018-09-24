29dcaac721ea70efa5275e85b373e17751567579
Mohos Tamás <tomi@mohos.name>
2018-09-24 13:02:41 +0200

    refactor(NikaCrawler): add some private method

--- src/DailyMenu/Crawler/NikaCrawler.php
+++ src/DailyMenu/Crawler/NikaCrawler.php
@@ -25,11 +25,7 @@ class NikaCrawler extends AbstractCrawler
         $postIndex = null;
         $posts = $domCrawler->filter('[data-sigil="expose"]');
         foreach($posts as $index => $postElement) {
-            if(strpos($postElement->textContent, $date->format('Y.m.d')) !== false) {
-                $dayNum = 0;
-            } else if(strpos($postElement->textContent, $date->format('m.d.')) !== false) {
-                $dayNum = 1;
-            }
+            $dayNum = $this->getDayNum($date, $postElement);
             if(null !== $dayNum) {
                 $postIndex = $index;
                 break;
@@ -44,13 +40,16 @@ class NikaCrawler extends AbstractCrawler
 
         $menuData = $this->getMenuData($post, $dayNum);
 
-        $foods = array_slice(explode(' - ', $menuData[0]->textContent), 1);
-        $dessert = explode(' - ', $menuData[1]->textContent, 2);
-        $foods[] = 'Desszert: ' . $dessert[1];
+        $foods = $this->getFoods($menuData);
 
         return new Menu($restaurant->getId(), $foods, $this->getPrice($post), $date);
     }
 
+    /**
+     * @param Crawler $post
+     * @param int $dayNum
+     * @return mixed
+     */
     private function getMenuData(Crawler $post, int $dayNum)
     {
         $menus = array_slice(iterator_to_array($post->filter('p')->getIterator()), 1);
@@ -58,10 +57,42 @@ class NikaCrawler extends AbstractCrawler
         return $dayChunks[$dayNum];
     }
 
+    /**
+     * @param Crawler $post
+     * @return int
+     */
     private function getPrice(Crawler $post): int
     {
         $priceMatches = [];
         preg_match('/Ár:.*([0-9]+)Ft/Us', $post->text(), $priceMatches);
         return (int)$priceMatches[1];
+    }
+
+    /**
+     * @param \DOMElement[] $menuData
+     * @return array
+     */
+    protected function getFoods(array $menuData)
+    {
+        $foods = array_slice(explode(' - ', $menuData[0]->textContent), 1);
+        $dessert = explode(' - ', $menuData[1]->textContent, 2);
+        $foods[] = 'Desszert: ' . $dessert[1];
+        return $foods;
+    }
+
+    /**
+     * @param DateTime $date
+     * @param $postElement
+     * @return int
+     */
+    protected function getDayNum(DateTime $date, \DOMElement $postElement): ?int
+    {
+        $dayNum = null;
+        if (strpos($postElement->textContent, $date->format('Y.m.d')) !== false) {
+            $dayNum = 0;
+        } elseif (strpos($postElement->textContent, $date->format('m.d.')) !== false) {
+            $dayNum = 1;
+        }
+        return $dayNum;
     }
 }
