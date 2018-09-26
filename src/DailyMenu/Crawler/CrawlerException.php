a03153696a94cb305c7131c7e4bcf086130b5fcd
Mohos Tamás <tomi@mohos.name>
2018-09-26 11:12:50 +0200

    feat(DailyMenu/Crawler): add CrawlerException

---
+++ src/DailyMenu/Crawler/CrawlerException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DailyMenu\Crawler;
+
+use RuntimeException;
+
+/**
+ * Class CrawlerException
+ * @package App\DailyMenu\Crawler
+ */
+class CrawlerException extends RuntimeException
+{
+
+}
